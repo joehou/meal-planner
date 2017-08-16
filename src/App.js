50bc16020606ce4f39907aaa667ab5e6cfbd1952
alsjoe <tyranthou@gmail.com>
2017-08-15 19:40:55 -0700

    finish react redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux'
-
+import {addRecipe, removeFromCalendar } from './actions'
 
 class App extends Component {
     render() {
@@ -22,8 +22,23 @@ function mapStateToProps (calendar) {
     return {
         calendar: dayOrder.map( day => ({
             day,
+            meals: Object.keys(calendar[day]).reduce((meals,meal) =>{
+                meals[meal] = calendar[day][meal]
+                ? calendar[day][meal]
+                : null
+
+                return meals
+            }, {})
         }))
     }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps (dispatch) {
+    return {
+        selectRecipe: (data) => dispatch(addRecipe(data)),
+        remove: (data) => dispatch(removeFromCalendar(data)),
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
